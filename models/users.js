f4902af05025e0d74e3308ea95bc1470ad91f75f
Claudia-teng <sydeng102@gmail.com>
2022-05-30 21:55:27 +0800

    delete unique custom err message in model

--- models/users.js
+++ models/users.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
     email: {
       type: String,
       required: [true, 'Please enter your email.'],
-      unique: [true, 'This email has signed up before.'],
+      unique: true,
       lowercase: true,
       select: false
     },
